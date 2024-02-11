859ac3e48439a5d9d0a61ccacb532daf619e6a95
Erfan <erfan.nemati@digikala.com>
2024-02-11 14:59:14 +0330

    chore: add mock data for drop down options

---
+++ src/constants/dropDownOptions.ts
@@ -0,0 +1,42 @@
+export const DROP_DOWN_OPTIONS = [
+  {
+    label: "Education",
+    id: 1,
+  },
+  {
+    label: "Science",
+    id: 2,
+  },
+  {
+    label: "Art",
+    id: 3,
+  },
+  {
+    label: "Sport",
+    id: 4,
+  },
+  {
+    label: "Games",
+    id: 5,
+  },
+  {
+    label: "Health",
+    id: 6,
+  },
+  {
+    label: "Option",
+    id: 7,
+  },
+  {
+    label: "Another Option",
+    id: 8,
+  },
+  {
+    label: "Option, Again",
+    id: 9,
+  },
+  {
+    label: "New Option",
+    id: 10,
+  },
+];
